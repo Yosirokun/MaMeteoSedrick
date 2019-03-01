1f8b6cdb0a90d23e46d7d936346711a4ef16d218
Sedrick <sedrickidd@hotmail.com>
2019-03-01 14:17:14 -0500

    mise a niveau de la base de donnees

--- MaMeteo/app/src/main/java/org/appducegep/mameteo/MeteoDAO.java
+++ MaMeteo/app/src/main/java/org/appducegep/mameteo/MeteoDAO.java
@@ -5,34 +5,43 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.text.format.DateFormat;
+import android.util.Log;
 
 import java.util.Date;
 
 // https://developer.android.com/training/data-storage/sqlite.html
 
 public class MeteoDAO extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "Meteo.db";
 
     public MeteoDAO(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    String SQL_CREATION_TABLE_1 = "create table meteo(id INTEGER PRIMARY KEY, ville TEXT, soleilOuNuage TEXT, date TEXT)";
+    String SQL_CREATION_TABLE = "create table meteo(id INTEGER PRIMARY KEY, ville TEXT, soleilOuNuage TEXT, date TEXT, vent TEXT)";
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String SQL_CREATION_TABLE = "create table meteo(id INTEGER PRIMARY KEY, ville TEXT, soleilOuNuage TEXT, date TEXT)";
+
         db.execSQL(SQL_CREATION_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int avant, int apres) {
-
+        Log.d("BASEDEDONNEES","MeteoDAO.onUpgrade() de " + avant + " a " + apres);
+        String SQL_MISEAJOUR_TABLE_A = "alter table meteo add column vent TEXT";
+        db.execSQL(SQL_MISEAJOUR_TABLE_A);
+        String SQL_MISEAJOUR_TABLE_B = "alter table meteo add column humidite INTEGER";
+        db.execSQL(SQL_MISEAJOUR_TABLE_B);
     }
 
     public void onDowngrade(SQLiteDatabase db, int avant, int apres) {
-        onUpgrade(db, avant, apres);
+        //onUpgrade(db, avant, apres);
+        Log.d("BASEDEDONNEES","MeteoDAO.onDowngrade() de " + avant + " a " + apres);
     }
 
     public void ajouterMeteo(String soleilOuNuage)
